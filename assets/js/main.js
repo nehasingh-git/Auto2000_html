7fb0815d1fabf82e3c83b8c9e7fdb4fb14ba3bc9
Neha Singh <nehasingh.apr12@gmail.com>
2023-02-03 20:31:55 +0530

    service flow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
-$(window).on('load', function() {
-  aos_init();
-});
 
-  function aos_init() {
-    AOS.init({
-      duration: 800,
-      once: true
-    });
+window.onload = function () {
+  aos_init();
+}
+
+// Init AOS
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false
+  });
+}
+
+document.querySelector('#btnRegistration').addEventListener('click', registrationVehicle);
+function registrationVehicle() {
+
+  var ele = document.querySelector("#inputRegistration");
+  if (!ele.value) {
+    alert("Please Enter Registration Number.")
   }
+
+  else if (ele.value.length < 7) {
+    alert("Please Enter Valid Registration Number.")
+  }
+  else {
+    window.location.href = "/services.html?regNo=" + ele.value ;
+  } 
+}
---
+++ assets/js/services.js
@@ -0,0 +1,50 @@
+
+window.onload = function () {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  let regNo = params.regNo; // "some_value"
+  if (regNo) {
+    test(regNo);
+  }
+}
+
+async function getRegistrationData(regNo) {
+  const response = await fetch("https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles", {
+    method: 'POST',
+    headers: {
+      'x-api-key': 'p8RCmO5r2l1JwiHIdbjao9In8f6uRltP6C1jEIfR',
+      'Content-Type': 'application/json'
+    },
+    body: `{
+      "registrationNumber": "TE57VRN"
+  }`,
+  });
+
+  response.json().then(data => {
+    console.log(data);
+  });
+}
+
+function test() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles");
+  xhr.setRequestHeader('x-api-key', 'p8RCmO5r2l1JwiHIdbjao9In8f6uRltP6C1jEIfR');
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+    }
+  };
+
+  let data = `{
+    "registrationNumber": "TE57VRN"
+}`;
+
+  xhr.send(data);
+}
+
